d015e7dd7b3b6963e6686bfdab848a461e6eb950
Rafael De Jesus Rebolledo <>
2023-08-23 16:36:23 -0600

    added dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.PGPORT,
+      username: process.env.PGUSER,
+      database: process.env.PGDATABASE,
+      password: process.env.PGPASSWORD,
+      autoLoadEntities: process.env.NODE_ENV === 'development',
+      synchronize: process.env.NODE_ENV === 'development',
+    }),
+    UserModule,
+  ],
   controllers: [],
   providers: [],
 })
